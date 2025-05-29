cfa063aade997190e756ac2bdfba5b6c08c2bc5f
Akeremyavuz <133967600+Akeremyavuz@users.noreply.github.com>
2025-05-29 12:59:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,20 @@ import 'package:mobiluygulamagelistirme/complaint_list_page.dart';
 import 'package:mobiluygulamagelistirme/complaint.dart';
 import 'package:mobiluygulamagelistirme/profile.dart';
 import 'complaint_data.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://supabase.com/dashboard/project/rldxceqyinumedzfptnq',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsZHhjZXF5aW51bWVkemZwdG5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1MDQ3MTEsImV4cCI6MjA2NDA4MDcxMX0.ITkluOrezbUujAAbXbHftOb-R5F9L-QP9WO2UeycRss',
+  );
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
